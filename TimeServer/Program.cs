2f1b7c56768e2824d15a3f71a02a83b0746ba85c
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-07-13 15:11:45 +0200

    Infinity looping tcplistener

--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -1,12 +1,20 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Net.Sockets;
 using System.Text;
 
 var tcpListener = new TcpListener(44444);
-tcpListener.Start();
 
-var AcceptClient = tcpListener.AcceptTcpClient();
-var currentStream = AcceptClient.GetStream();
-var bytes = Encoding.ASCII.GetBytes("Hello");
-currentStream.Write(bytes);
-currentStream.Close();
-AcceptClient.Close();
+
+while (true)
+{
+     tcpListener.Start();
+     var AcceptClient = tcpListener.AcceptTcpClient();
+     var currentStream = AcceptClient.GetStream();
+     var DT = DateTime.Now;
+     var bytes = Encoding.ASCII.GetBytes($"Hello {DT}");
+     currentStream.Write(bytes);
+     currentStream.Close();
+     AcceptClient.Close();
+}
+
+
