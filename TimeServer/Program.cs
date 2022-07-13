781b02f60f4bb3ae5bee3ff0c1bbd977a7e7b55d
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-07-13 14:36:24 +0200

    TimeServer return hello once

--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text;
+
+var tcpListener = new TcpListener(44444);
+tcpListener.Start();
+
+var AcceptClient = tcpListener.AcceptTcpClient();
+var currentStream = AcceptClient.GetStream();
+var bytes = Encoding.ASCII.GetBytes("Hello");
+currentStream.Write(bytes);
+currentStream.Close();
+AcceptClient.Close();
