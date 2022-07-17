15ae2d8793b419929ed1e65a5598023a64aab565
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-07-17 18:55:42 +0200

    not sure but i feel like i made a step, but i cant really know for sure, unity freezes when i try to test play it

--- Agario/Assets/Scripts/RequestServerTime.cs
+++ Agario/Assets/Scripts/RequestServerTime.cs
@@ -10,24 +10,28 @@ using UnityEngine.UI;
 
 public class RequestServerTime : MonoBehaviour
 {
-   public Text textObject;
-   public Button button;
+   public Text txtobject;
    public void SendRequest()
    {
       var Client = new TcpListener(44444);
-      Client.Start();
+      
+      while (true)
+      {
+         Client.Start();
+         var acceptClient = Client.AcceptTcpClient();
+         var currentStream = acceptClient.GetStream();
+         var currentDT = DateTime.Now;
+         var bytes = Encoding.ASCII.GetBytes($"hello {currentDT}");
+         currentStream.Read(bytes);
 
-      var acceptClient = Client.AcceptTcpClient();
-      var currentStream = acceptClient.GetStream();
-      var currentDT = DateTime.Now;
-      var bytes = Encoding.ASCII.GetBytes($"hello {currentDT}"); 
-      currentStream.Read(bytes);
-   }
-
-   private void Start()
-   {
-      SendRequest();
+         var txt = txtobject;
+         txt.text = currentStream.Read(bytes).ToString();
+      }
       
    }
 
+   public void Start()
+   {
+      SendRequest();
+   }
 }
