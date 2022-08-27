6d46a4e8db186a61e3acb51fc3ca67f432927e95
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-08-27 18:05:20 +0200

    Now it can receive data from udpClient

--- OpenWord-MMO/Program.cs
+++ OpenWord-MMO/Program.cs
@@ -1,3 +1,10 @@
-﻿using System.Net.Sockets;
+﻿using System.Net;
+using System.Net.Sockets;
 
 UdpClient port = new UdpClient(444446);     // Created port
+
+while (true)
+{
+    var EndPoint = new IPEndPoint(IPAddress.Any, 11000);
+    var data = port.Receive(ref EndPoint);
+}
