3fa123d360a3d6513a002d2c80d6c426d164d39d
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-08-27 17:52:06 +0200

    Port Created

--- OpenWord-MMO/Program.cs
+++ OpenWord-MMO/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+
+UdpClient port = new UdpClient(444446);     // Created port
