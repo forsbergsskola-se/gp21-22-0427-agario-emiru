6ebe2bdd581013e2f9d1606813f435bd46f95974
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-08-27 22:45:00 +0200

    server is working kidna but not fully
    Problem: whenever i sen a message, it says "unhandled exceptions"

--- OpenWord-MMO/Program.cs
+++ OpenWord-MMO/Program.cs
@@ -3,21 +3,16 @@ using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
+using System.Text;
 
-//TODO: Need Constructor to pass port number
-
-//TODO: Receiver Method
-//TODO: Send Method that sends data on that socket
-//TODO:Close everything when done
-
-var port = Console.ReadLine();
-string data = "";
-
-UdpClient Client = new UdpClient();     // Created port
-var remoteEP = new IPEndPoint(IPAddress.Any, 44445);
+var message = "";
+UdpClient Client = new UdpClient(11114);
 
 while (true)
 {
-    var receive = Client.Receive(ref remoteEP);
-    
+    var remoteEP = new IPEndPoint(IPAddress.Any, 11114);
+    var data = Client.Receive(ref remoteEP);
+
+    Client.Send(data, data.Length, remoteEP);
+    Client.Close();
 }
