327c82f7d0a4e7264ddde6e9ececd3ce0762653c
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-08-28 19:09:24 +0200

    made a valid statment, if the word is longer then 20 int, it will break

--- OpenWord-MMO/Program.cs
+++ OpenWord-MMO/Program.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
 
+var Message = "Hello";
 UdpClient Client = new UdpClient(11114);
 
 while (true)
 {
-    
     var remoteEP = new IPEndPoint(IPAddress.Any, 11114);
     var data = Client.Receive(ref remoteEP);
-    
-    
-    var sender = Client.Send(data, data.Length, remoteEP);
-    var message = Encoding.ASCII.GetString(data);
-    Console.WriteLine(message.Length);
 
+    var sender = Client.Send(data, data.Length, remoteEP);
+    Console.WriteLine(sender);
+
+    if (sender > 20)
+    {
+        break;
+    }
+
+    if (sender < 20)
+    {
+        for (int i = 0; i > 1;)
+        {
+            Console.WriteLine(" ");
+        }
+        
+    }
 }
 Client.Close();
