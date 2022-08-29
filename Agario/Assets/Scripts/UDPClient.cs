a81d3251ed023798f0ca787a3bd1a96b740f39d0
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-08-29 21:27:07 +0200

    trying to make use of bool and button class

--- Agario/Assets/Scripts/UDPClient.cs
+++ Agario/Assets/Scripts/UDPClient.cs
@@ -5,12 +5,16 @@ using System.Net.Sockets;
 using System.Text;
 using Mono.Cecil;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class UDPClient : MonoBehaviour
 {
     public string message = "";
-    
+    public Text txt;
+    public Button Button;
+    public bool buttonClicked;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +24,20 @@ public class UDPClient : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var remoteEP = new IPEndPoint(IPAddress.Any, 11113);
-        UdpClient client = new UdpClient(11113);
-
+        UdpClient client = new UdpClient(11113);    // Client created
+        var remoteEP = new IPEndPoint(IPAddress.Any, 11113);    //EndPoint created
         var data = client.Receive(ref remoteEP);
-        var send = client.Send(data, data.Length, remoteEP);
-        var sent = Encoding.ASCII.GetString();
-        var read = sent;
-        Debug.Log(read);
+        var datta = Encoding.ASCII.GetString(data);
+        var sent = message += datta;
+
+        var bytte = Encoding.ASCII.GetBytes(sent);
+        var sender = client.Send(bytte, bytte.Length, remoteEP);
+        
+        Debug.Log(sender);
+
+        if ()
+        {
+            
+        }
     }   
 }
